a6fda67588873e208fcc47c04cc7c67c70b2664f
Kodik77rus <plyhach@gmail.com>
2021-03-10 18:19:46 +0300

    connect to db

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const URL =  require('./utils')
 
 const PORT = process.env.PORT || 3000
 
@@ -6,6 +8,15 @@ const app = express()
 
 async function start() {
   try {
+    await mongoose.connect(
+      URL,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true
+      }
+    )
     app.listen(PORT, () => {
       console.log(`App listening at http://localhost:${PORT}`)
     })
