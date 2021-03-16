576b02ec19286c7334b5369cbd923facaf4b3eb3
Kodik77rus <plyhach@gmail.com>
2021-03-16 19:15:42 +0300

    mini refactor

--- utils/index.js
+++ utils/index.js
@@ -1,3 +1,2 @@
-const URL_FOR_CONNECT_TO_DB = 'mongodb+srv://george:Z34vE8Nm3xaiifoF@cluster0.fihkq.mongodb.net/ads?retryWrites=true&w=majority'
+exports.URL_FOR_CONNECT_TO_DB = 'mongodb+srv://george:Z34vE8Nm3xaiifoF@cluster0.fihkq.mongodb.net/ads?retryWrites=true&w=majority'
 
-module.exports = URL_FOR_CONNECT_TO_DB
