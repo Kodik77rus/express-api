5d97702838db88fc4e8f9149dea16d296dba225d
Kodik77rus <plyhach@gmail.com>
2021-07-19 01:56:31 +0300

    create main layout

---
+++ firstAPI-frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import SiteNavbar from './components/navbar'
+import CardGroup from './components/cards'
+
+function App() {
+  return (
+    <div className="App">
+      <SiteNavbar />
+      <CardGroup />
+    </div>
+  )
+}
+
+export default App;
